729ceed935231aaae89e0c08d5d75ae9456beacb
colynn.liu <colynn.liu@gmail.com>
2020-10-30 11:18:53 +0800

    add bubble sort

---
+++ README.md
@@ -0,0 +1,4 @@
+## go-algorithm
+
+## Features
+[ ]
---
+++ bubble_sort_test.go
@@ -0,0 +1,15 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBubbleSort(t *testing.T) {
+	arr := []int{3, 2, 0, 5, 10, 1}
+	arrSort := BubbleSort(arr)
+	fmt.Printf("arr after sored: %v\n", arrSort)
+	assert.Equal(t, 10, arrSort[5])
+}
