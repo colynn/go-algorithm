29f8525fb4ba7976f28842a6ff2b5730de157f7b
colynn.liu <colynn.liu@gmail.com>
2020-12-07 11:43:55 +0800

    add shell sort

--- shell_sort.go
+++ shell_sort.go
@@ -8,5 +8,19 @@ package algorithm
 // 3.每趟排序，根据对应的增量 ti，将待排序列分割成若干长度为 m 的子序列，分别对各子表进行直接插入排序。
 // 仅增量因子为 1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
 func ShellSort(arr []int) []int {
+	length := len(arr)
+	gap := length / 2
+	for gap > 0 {
+		for i := gap; i < length; i++ {
+			temp := arr[i]
+			j := i - gap
+			for j >= 0 && arr[j] > temp {
+				arr[j+gap] = arr[j]
+				j -= gap
+			}
+			arr[j+gap] = temp
+		}
+		gap = gap / 2
+	}
 	return arr
 }
